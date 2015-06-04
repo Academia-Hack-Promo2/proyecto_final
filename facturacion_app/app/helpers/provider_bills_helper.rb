f074deea3c6a863abd7db778d212c0888aa488b4
samuel melgarejo <samuel_021093@hotmail.com>
2015-06-04 13:32:09 -0430

    generado todos los controladores

---
+++ facturacion_app/app/helpers/provider_bills_helper.rb
@@ -0,0 +1,2 @@
+module ProviderBillsHelper
+end
---
+++ facturacion_app/test/controllers/provider_bills_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class ProviderBillsControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end

f20176b203dbc573f22cef0eaeb335c2a16dbf08
samuel melgarejo <samuel_021093@hotmail.com>
2015-06-06 12:48:38 -0430

    contraladores de bills y provider_bills

--- facturacion_app/app/controllers/bills_controller.rb
+++ facturacion_app/app/controllers/bills_controller.rb
@@ -1,2 +1,37 @@
 class BillsController < ApplicationController
+  def index
+    @bills = Bill.all
+  end
+  def new
+    @bill = Bill.new
+  end
+  def create
+    @bill = Bill.new(permit_params)
+    if @bill.save
+      redirect_to Bills_path
+    else
+      reditect 'new'
+    end
+  end
+  def edit
+    @bill = Bill.find(params[:id])
+  end
+  def update
+    @bill = Bill.find(params[:id])
+    if @bill.update(permit_params)
+      redirect_to bills_path
+    else
+      redirect 'new'
+    end
+  end
+  def destroy
+    @bill = Bill.find(params[:id])
+    if @bill.delete
+      redirect_to bills_path
+    end
+  end
+  private
+    def permit_params
+      params.require(:bill).permit(:bill_number, :bill_control, :payment, :client_rif, :client_name, :client_phone, :client_email, :quantity, :unit_price, :tax, :bill_total)
+    end
 end
--- facturacion_app/app/controllers/provider_bills_controller.rb
+++ facturacion_app/app/controllers/provider_bills_controller.rb
@@ -1,2 +1,37 @@
 class ProviderBillsController < ApplicationController
+	def index
+    @p_bills = ProviderBills.all
+  end
+  def new
+    @p_bill = ProviderBills.new
+  end
+  def create
+    @p_bill = ProviderBillsl.new(permit_params)
+    if @p_bill.save
+      redirect_to Bills_path
+    else
+      reditect 'new'
+    end
+  end
+  def edit
+    @p_bill = ProviderBills.find(params[:id])
+  end
+  def update
+    @p_bill = ProviderBills.find(params[:id])
+    if @p_bill.update(permit_params)
+      redirect_to provider_bills_path
+    else
+      redirect 'new'
+    end
+  end
+  def destroy
+    @p_bill = ProviderBills.find(params[:id])
+    if @p_bill.delete
+      redirect_to provider_bills_path
+    end
+  end
+  private
+    def permit_params
+      params.require(:providerbill).permit(:bill_number, :amount, :payment_number, :provider)
+    end
 end
